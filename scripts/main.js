1032928afc43503e49f4b1353eea05b68473dc85
Jason Kim <jason.s.kim95@gmail.com>
2021-09-30 17:36:34 -0400

    Add game control - switch player when turn ends

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,38 @@
 let gameBoard = (function() {
-    let board = ['x', 'o', 'o', 'o', 'x', 'o', 'o', 'x', 'x'];
+    //let board = ['x', 'o', 'o', 'o', 'x', 'o', 'o', 'x', 'x'];
     let htmlBoard = document.querySelectorAll('.column');
-
     return {htmlBoard};
 })();
 
-let gameControl = (function() {
-
-})();
-
 let Player = (name, mark, turn) => {
-    const getName = () => name;
-    const getMark = () => mark;
-    const isTurn = () => turn;
-    const _makeMove = (cell) => {
-        return function() {cell.textContent = mark};
+    function makeMove(cell) {
+        cell.textContent = mark
     }
 
-    let i = 0;
-    gameBoard.htmlBoard.forEach(cell => {
-        cell.addEventListener('click', _makeMove(cell));
-    });
-
-    return {getName, getMark, isTurn};
+    return {
+        name: name,
+        mark: mark,
+        turn: turn,
+        makeMove};
 };
 
-const jason = Player('Jason', 'X', true);
+let gameControl = (function() {
+    const playerOne = Player('Jason', 'X', true);
+    const playerTwo = Player('Computer', 'O', false);
+    
+    function _getCurrentPlayer() {
+        return playerOne.turn ? playerOne : playerTwo;
+    };
+
+    function _endTurn() {
+        playerOne.turn = !playerOne.turn;
+        playerTwo.turn = !playerTwo.turn;
+    }
+
+    gameBoard.htmlBoard.forEach(cell => {
+        cell.addEventListener('click', () => {
+            _getCurrentPlayer().makeMove(cell);
+            _endTurn();
+        });
+    });
+})();
