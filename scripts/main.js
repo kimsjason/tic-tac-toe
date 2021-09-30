7371b8a813f9bb79aca865725407b7839b480680
Jason Kim <jason.s.kim95@gmail.com>
2021-09-30 11:32:26 -0400

    Create initial Player factory

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,10 @@ let gameBoard = (function() {
     let board = [['x', 'o', 'o'],['o', 'x', 'o'],['o', 'x', 'x']];
     board.forEach(row => console.log(row));
 })();
+
+let Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    
+    return {getName, getMark};
+};
