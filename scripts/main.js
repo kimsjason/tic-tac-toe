615f5af584b564cb5fde8b05a0591682c4da1af1
Jason Kim <jason.s.kim95@gmail.com>
2021-10-01 13:40:27 -0400

    Add check winner function for rows/columns/diagonals

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,62 @@
 let gameBoard = (function() {
-    //let board = ['x', 'o', 'o', 'o', 'x', 'o', 'o', 'x', 'x'];
     let htmlBoard = document.querySelectorAll('.column');
+    
     return {htmlBoard};
 })();
 
 let Player = (name, mark, turn) => {
+    let moves = [];
+
     function makeMove(cell) {
         if (cell.textContent == '') {
-            cell.textContent = mark
-            endTurn();
+            cell.textContent = mark;
+            moves.push([cell.parentElement.className, cell.className]);
+            _checkWinner();
+            _endTurn();
         }
     }
 
-    function endTurn() {
+    function _endTurn() {
         gameControl.playerOne.turn = !gameControl.playerOne.turn;
         gameControl.playerTwo.turn = !gameControl.playerTwo.turn;
     }
+    
+    function _checkWinner() {
+        const reducer = function(uniqueMoves, move) {
+            // Counts unique rows
+            if (move[0] in uniqueMoves) {
+                uniqueMoves[move[0]]++
+            } else {
+                uniqueMoves[move[0]] = 1;
+            }
+            // Counts unique columns
+            if (move[1] in uniqueMoves) {
+                uniqueMoves[move[1]]++
+            } else {
+                uniqueMoves[move[1]] = 1;
+            }
+            return uniqueMoves;
+        }
+
+        const uniqueMoves = moves.reduce(reducer, {});
+        console.log(uniqueMoves);
+        for (moveType in uniqueMoves) {
+            // Checks 3 in a row for rows & columns
+            if (uniqueMoves[moveType] == 3) {
+                console.log('winner winner, chicken dinner!!');
+            } 
+            // Checks 3 in a row for diagonals - there has to be one count for every row & column
+            else if (Object.keys(uniqueMoves).length == 6) {
+                console.log('winner winner chicken dinner');
+            }
+        }
+    }   
     
     return {
         name: name,
         mark: mark,
         turn: turn,
+        moves: moves,
         makeMove
     };
 };
